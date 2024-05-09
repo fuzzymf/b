9ed376da1e2836f68863033890b9fa29bf472485
Anubhab Patnaik <anubhabr50@gmail.com>
2024-05-09 19:42:54 +0530

    a working version with webtorrent

--- src/bencodeparser.ts
+++ src/bencodeparser.ts
@@ -19,44 +19,37 @@ export class Torrent {
     }
 }
 export function parseMagnetLink(sourcelink: string): Torrent {
-    var link: string = ""
-    if (fs.existsSync(sourcelink)) {
+    console.log("sourcelink: " + sourcelink + "\n+" + sourcelink.startsWith('magnet'));
+    if (sourcelink.startsWith('magnet')) {
+        const xtMatch = sourcelink.match(/xt=urn:btih:([a-fA-F0-9]{40})/);
+        if (!xtMatch) {
+            throw new Error('Invalid magnet sourcelink: missing xt parameter');
+        }
+        const infoHash = xtMatch[1];
 
-        // const torrent = fs.readFileSync(sourcelink);
-        // throw new Error('File reading not implemented yet. Please use a magnet link instead.');
-        // link = torrent.toString();
+        const displayNameMatch = sourcelink.match(/dn=([^&]+)/);
+        const displayName = displayNameMatch ? decodeURIComponent(displayNameMatch[1]) : undefined;
+
+        const announceMatch = sourcelink.match(/tr=([^&]+)/);
+        const announce = announceMatch ? decodeURIComponent(announceMatch[1]) : undefined;
+
+        const announceListMatch = sourcelink.match(/tr=([^&]+)/g);
+        const announceList = announceListMatch ? announceListMatch.map((match) => decodeURIComponent(match.substr(3))) : undefined;
+
+        const sizeMatch = sourcelink.match(/xl=([^&]+)/);
+        const size = sizeMatch ? parseInt(sizeMatch[1]) : undefined;
+
+        const trackerMatches = sourcelink.match(/tr=([^&]+)/g);
+        const trackers = trackerMatches ? trackerMatches.map((match) => decodeURIComponent(match.substr(3))) : undefined;
+
+        const urlListMatch = sourcelink.match(/ws=([^&]+)/);
+        const urlList = urlListMatch ? [decodeURIComponent(urlListMatch[1])] : undefined;
+        console.log("infoHash: " + infoHash);
+        console.log("displayName: " + displayName);
+        console.log("trackers: " + trackers);
+        return new Torrent(infoHash, displayName, trackers, urlList);
+    } else {
+        throw new Error('Invalid magnet link provided');
     }
-    else if (sourcelink.startsWith('magnet')) link = sourcelink;
-    else throw new Error('Invalid torrent file provided');
-    
-    const xtMatch = link.match(/xt=urn:btih:([a-fA-F0-9]{40})/);
-    if (!xtMatch) {
-        throw new Error('Invalid magnet link: missing xt parameter');
-    }
-    const infoHash = xtMatch[1];
-
-    const displayNameMatch = link.match(/dn=([^&]+)/);
-    const displayName = displayNameMatch ? decodeURIComponent(displayNameMatch[1]) : undefined;
-
-    const announceMatch = link.match(/tr=([^&]+)/);
-    const announce = announceMatch ? decodeURIComponent(announceMatch[1]) : undefined;
-
-    const announceListMatch = link.match(/tr=([^&]+)/g);
-    const announceList = announceListMatch ? announceListMatch.map((match) => decodeURIComponent(match.substr(3))) : undefined;
-
-    const sizeMatch = link.match(/xl=([^&]+)/);
-    const size = sizeMatch ? parseInt(sizeMatch[1]) : undefined;
-
-    const trackerMatches = link.match(/tr=([^&]+)/g);
-    const trackers = trackerMatches ? trackerMatches.map((match) => decodeURIComponent(match.substr(3))) : undefined;
-
-    const urlListMatch = link.match(/ws=([^&]+)/);
-    const urlList = urlListMatch ? [decodeURIComponent(urlListMatch[1])] : undefined;
-
-
-    // console.log("infoHash: " + infoHash);
-    // console.log("displayName: " + displayName);
-    // console.log("trackers: " + trackers);
-    return new Torrent(infoHash, displayName, trackers, urlList);
 }
 // "magnet:?xt=urn:btih:247EB3694D3E3E4A8879F1C4B85A33D1876283B0&dn=Ratiborus+KMS+Tools+v01.06.2021+%28Activate+Windows+and+MS+Office%29&tr=http%3A%2F%2Fp4p.arenabg.com%3A1337%2Fannounce&tr=udp%3A%2F%2F47.ip-51-68-199.eu%3A6969%2Fannounce&tr=udp%3A%2F%2F9.rarbg.me%3A2780%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2710%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2730%2Fannounce&tr=udp%3A%2F%2F9.rarbg.to%3A2920%2Fannounce&tr=udp%3A%2F%2Fopen.stealth.si%3A80%2Fannounce&tr=udp%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.cyberia.is%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.dler.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.internetwarriors.net%3A1337%2Fannounce&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=udp%3A%2F%2Ftracker.pirateparty.gr%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.tiny-vps.com%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.torrent.eu.org%3A451%2Fannounce"
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,37 @@
-import {downloadTorrent} from './tcpclient.js';
+import { downloadTorrent } from './tcpclient.js';
 import { existsSync } from 'fs';
 import { parseMagnetLink, Torrent } from './bencodeparser.js';
-import downloadFile from './downloadFile.js';
+import downloadFile from './torrent_file.js';
+import downloadMagnet from './magnet_link.js';
 
 const arg: string = process.argv[2];
 
 
 switch (arg) {
 	case '-h':
-		console.log('Usage: node src/index.js [torrent file path]');
-		console.log('Usage: node src/index.js [magnet link]');
+		console.log('Usage: node src/index.js -- -t [torrent file path]');
+		console.log('Usage: node src/index.js -- -m [magnet link]');
 		break;
 	case '-v':
 		console.log('Version: 1.0.0');
-		break;	
+		break;
+	case '-m':
+		const link: string = process.argv[3];
+		if (link.startsWith('magnet')) {
+			downloadMagnet(link);
+		}
+		else throw new Error('Invalid magnet link provided');
+		break;
+	case '-t':
+		const file: string = process.argv[3];
+		if (existsSync(file)) {
+			downloadFile(file);
+		}
+		else throw new Error('Invalid torrent file provided');
+		break;
+	default:
+		console.log('Invalid argument provided\n');
+		console.log('Usage: npm run bdown -- -t [torrent file path]\n OR \nUsage: node src/index.js -- -m [magnet link]');
+		break;
 }
 
-if (arg.startsWith('magnet')) {
-	const magnet: Torrent = parseMagnetLink(arg);
-	downloadTorrent(magnet);
-}
-else {
-	if (existsSync(arg)) {
-		// const torrent: Torrent = parseMagnetLink(arg);
-		// downloadTorrent(torrent);
-		downloadFile(arg);
-	}
-	else if(!arg) throw new Error('No torrent file provided')
-	else throw new Error('Invalid torrent file provided')
-}
