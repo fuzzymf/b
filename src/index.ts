e6615258da04edc2691f464abc3706b7aca879db
Anubhab Patnaik <anubhabr50@gmail.com>
2024-11-30 13:23:49 -0500

    feat: new start

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,44 @@
-import { downloadTorrent } from './client.js';
 import { existsSync } from 'fs';
-import { parseMagnetLink, Torrent } from './bencodeparser.js';
-import downloadFile from './webt/torrent_file.js';
-import downloadMagnet from './webt/magnet_link.js';
+import { parseMagnetLink } from './bencodeparser.js';
+import { Torrent } from './torrent.js';
 
 const arg: string = process.argv[2];
 
 
 switch (arg) {
 	case '-h':
-		console.log('Usage: node src/index.js -- -t [torrent file path]');
-		console.log('Usage: node src/index.js -- -m [magnet link]');
+		console.info('Usage: bee -- -t [torrent file path]');
+		console.info('Usage: bee -- -m [magnet link]');
 		break;
 	case '-v':
-		console.log('Version: 1.0.0');
+		console.log('Version: 1.1.0');
 		break;
 	case '-m':
-		const link: string = process.argv[3];
-		if (link.startsWith('magnet')) {
-			downloadMagnet(link);
-			// const torrent: Torrent = parseMagnetLink(link);
-			// downloadTorrent(torrent);
+		try {
+			const link: string = process.argv[3];
+			if (link.startsWith('magnet')) {
+				const torrent: Torrent = parseMagnetLink(link);
+				// torrent.download();
+			}
+			else throw new Error('Invalid magnet link provided');
+			break;
+		} catch (error) {
+			console.error(error);
 		}
-		else throw new Error('Invalid magnet link provided');
-		break;
 	case '-t':
-		const file: string = process.argv[3];
-		if (existsSync(file)) {
-			downloadFile(file);
+		const filePath: string = process.argv[3];
+		try {
+			if (existsSync(filePath)) {
+				// Handle decoding of torrent file
+				// const torrent: Torrent = parseTorrentFile(filePath);
+				// torrent.download();
+			}
+			else throw new Error('Invalid torrent file provided');
+			break;
 		}
-		else throw new Error('Invalid torrent file provided');
-		break;
+		catch (error) {
+			console.error(error);
+		}
 	default:
 		console.log('Invalid argument provided\n');
 		console.log('Usage: npm run bdown -- -t [torrent file path]\n OR \nUsage: node src/index.js -- -m [magnet link]');
